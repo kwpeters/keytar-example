cd5533515d4238d8a248a419a3d369386a761f9e
Kevin W. Peters <kwpeters@ra.rockwell.com>
2019-03-26 16:08:32 -0400

    Added code to add, remove and run service.

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+const service = require("os-service");
 
-app.get("/", function (req, res){
-    res.send("hello world");
-});
 
-app.listen(3000);
+const PORT_NUM = 3000;
+
+
+if (process.argv[2] === "--add") {
+    service.add("keytar-example-service", {programArgs: ["--run"]}, function (err) {
+        if (err) {
+            console.trace(err);
+        }
+    });
+}
+else if (process.argv[2] === "--remove") {
+    service.remove("keytar-example-service", function (err) {
+        if (err) {
+            console.trace(err);
+        }
+    });
+}
+else if (process.argv[2] === "--run") {
+    service.run(function () {
+        service.stop(0);
+    });
+
+    // Service code...
+    app.get("/", function (req, res){
+        res.send("hello world");
+    });
+
+    app.listen(PORT_NUM);
+
+}
+else {
+    console.trace("TODO: Show usage.");
+}
